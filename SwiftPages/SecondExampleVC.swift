ef9510b7abcaf90e63df70b5d52c0667710129f6
AmosKing <wangjiuxing2010@hotmail.com>
2016-01-20 16:55:48 +0800

    fixed coding style

--- SwiftPages/SecondExampleVC.swift
+++ SwiftPages/SecondExampleVC.swift
@@ -9,38 +9,21 @@
 import UIKit
 
 class SecondExampleVC: UIViewController {
-
+    
     @IBOutlet var swiftPagesView: SwiftPages!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Initiation
-        let VCIDs : [String] = ["FirstVC", "SecondVC", "ThirdVC", "FourthVC", "FifthVC"]
-        let buttonTitles : [String] = ["Home", "Places", "Photos", "List", "Tags"]
+        // Initiation
+        let VCIDs = ["FirstVC", "SecondVC", "ThirdVC", "FourthVC", "FifthVC"]
+        let buttonTitles = ["Home", "Places", "Photos", "List", "Tags"]
         
-        //Sample customization
+        // Sample customization
         swiftPagesView.setOriginY(0.0)
         swiftPagesView.enableAeroEffectInTopBar(true)
         swiftPagesView.setButtonsTextColor(UIColor.whiteColor())
         swiftPagesView.setAnimatedBarColor(UIColor.whiteColor())
         swiftPagesView.initializeWithVCIDsArrayAndButtonTitlesArray(VCIDs, buttonTitlesArray: buttonTitles)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
